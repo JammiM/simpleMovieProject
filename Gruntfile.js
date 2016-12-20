7f1123cba40470d369f2915c4cb129ddd14d0c22
JammiM <jammim19@gmail.com>
2016-12-20 14:14:02 +0000

    Enable all the plugins within the gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,5 +6,9 @@ module.exports = function(grunt) {
 // Load grunt plugins.
 
  grunt.loadNpmTasks('grunt-contrib-pug');
+ grunt.loadNpmTasks('grunt-autoprefixer');
+ grunt.loadNpmTasks('grunt-contrib-uglify');
+ grunt.loadNpmTasks('grunt-contrib-watch');
+
  
 };

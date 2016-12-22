e6db3cf656763acdaaa605fb68d54149b013a097
JammiM <jammim19@gmail.com>
2016-12-22 10:16:59 +0000

    Added 'Ulgify' and an 'watch' component to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,57 @@
-// The basic shell 
-
-
 module.exports = function(grunt) {
 
-// Load grunt plugins.
+	// Seting up some configuration.
+	grunt.initConfig({
 
- grunt.loadNpmTasks('grunt-contrib-pug');
- grunt.loadNpmTasks('grunt-autoprefixer');
- grunt.loadNpmTasks('grunt-contrib-uglify');
- grunt.loadNpmTasks('grunt-contrib-watch');
+		pug: {
+		  compile: {
+		    files: [{
+		      expand: true,
+		      cwd: "source/jade",
+		      src: "*.jade",
+		      dest: "build",
+		      ext: ".html"
+		    }]
+		  }
+		},
 
- 
-};
+
+		//Grabs both JQuery and modernizr and merges them into one.
+		uglify: {
+		  bower_js_files: {
+		    files: {
+		      'build/output.min.js': [
+		        'bower_components/jquery/dist/jquery.js',
+		        'bower_components/modernizr/modernizr.js'
+		      ]
+		    }
+		  }
+		},
+
+
+
+
+		// Watches for changes in code.
+		watch: {
+		  pug: {
+		    files: [ 'source/jade/*.jade' ],
+		    tasks: ['jade']
+		  }
+		},
+
+
+
+
+	 
+	});//grunt.initConfig
+
+
+
+
+    // Loading the grunt plugins.
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+ 	grunt.loadNpmTasks('grunt-contrib-uglify');
+ 	grunt.loadNpmTasks('grunt-contrib-watch');
+
+};//module.exports
